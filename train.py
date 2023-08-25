2f89627eef62da5f796b90863eaba68069c65a35
waterq <dongworld@gmail.com>
2023-08-25 01:46:47 +0000

    Modify train.py

--- train.py
+++ train.py
@@ -25,21 +25,25 @@ from src.preprocess import preprocess_pipeline
 
 # 로그 들어갈 위치
 # 로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
+logger = set_logger(os.path.join(LOG_FILEPATH, "logs.log"))
 
 sys.excepthook = handle_exception
 warnings.filterwarnings(action="ignore")
 
 
 if __name__ == "__main__":
+    logger.info("Loading Data...")
     train_df = pd.read_csv(os.path.join(DATA_PATH, "house_rent_train.csv"))
 
     _X = train_df.drop(["rent", "area_locality", "posted_on"], axis=1)
     y = np.log1p(train_df["rent"])
 
     # X=_X, y=y로 전처리 파이프라인을 적용해 X에 저장
+    logger.info("Applying a pipeline...")
     X = preprocess_pipeline.fit_transform(X=_X, y=y)
 
     # Data storage - 피처 데이터 저장
+    logger.info("Saving a Data storage...")
     if not os.path.exists(os.path.join(DATA_PATH, "storage")):
         os.makedirs(os.path.join(DATA_PATH, "storage"))
     X.assign(rent=y).to_csv(
@@ -61,6 +65,7 @@ if __name__ == "__main__":
     mlflow.set_experiment(experiment_name=experiment_name)
     mlflow.set_tracking_uri("./mlruns")
 
+    logger.debug("Using Train...")
     for i, params in enumerate(param_set):
         run_name = f"Run {i}"
         with mlflow.start_run(run_name=f"Run {i}"):
@@ -121,6 +126,7 @@ if __name__ == "__main__":
 
     best_run = mlflow.get_run(best_run_df.at[0, "run_id"])
     best_params = best_run.data.params
+    # logger.info(f"Best Param {best_params}")
 
     best_model_uri = f"{best_run.info.artifact_uri}/model"
 
